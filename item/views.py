1f0738883f5dbac7e8c15ca51c2d5717149b96f9
Zoranmihov <mihov.zoran@hotmail.com>
2023-03-04 08:23:48 +0100

    Added product details

--- item/views.py
+++ item/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
+import random
 
-# Create your views here.
+#  Create your views here.
+
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)
+    random_items = related.order_by('?')[:10]
+
+    return render(request, 'item/detail.html', {'item': item, "related":random_items})
